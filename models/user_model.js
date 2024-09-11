236b5b397e00d227b8756a36cdb1684c25d95bef
naji2-2 <s2322@e-mirim.hs.kr>
2024-09-11 19:06:33 +0900

    feat: user_model 추가

---
+++ models/user_model.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        name: { 
+            type: DataTypes.STRING(255),
+            required: true 
+        },
+        baseball_team_name: {
+            type: DataTypes.INTEGER
+        },
+        image_url: {
+            type: DataTypes.TEXT
+        },
+        kakao_id: {
+            type: DataTypes.INTEGER,
+            required: true,
+            unique: true
+        }
+    }, {
+        tableName: 'user_information',
+        timestamps: true,
+        underscored: true,
+    });
+    return User;
+};
